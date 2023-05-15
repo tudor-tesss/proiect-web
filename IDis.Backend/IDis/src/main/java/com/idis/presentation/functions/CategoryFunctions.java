bf5841d84d5b2fc994a0419cc3ca3f9a1feefe7b
Stirbu.Larisa <eusuntlarisa@gmail.com>
2023-05-15 22:33:13 +0300

    category functions

---
+++ IDis.Backend/IDis/src/main/java/com/idis/presentation/functions/CategoryFunctions.java
@@ -0,0 +1,38 @@
+package com.idis.presentation.functions;
+
+import com.idis.core.business.commands.category.CreateCategoryCommand;
+import com.idis.core.business.commands.user.*;
+import com.nimblej.core.Function;
+import com.nimblej.core.IUserController;
+import com.nimblej.core.Mediator;
+import com.nimblej.extensions.json.Serialization;
+import com.nimblej.networking.http.communication.HttpResponse;
+import com.nimblej.networking.http.communication.HttpVerbs;
+import com.nimblej.networking.http.routing.Route;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+public class CategoryFunctions implements IUserController {
+
+    private static Mediator mediator = Mediator.getInstance();
+
+    @Route(path= "/categories", method = HttpVerbs.POST)
+    @Function(name = "createCategory")
+    public static CompletableFuture <HttpResponse> createCategory (String requestBody){
+        var command = Serialization.deserialize(requestBody, CreateCategoryCommand.class);
+
+        try{
+            return mediator
+                    .send(command)
+                    .thenCompose(category -> {
+                        var responseContent = Serialization.serialize(category);
+
+                        return HttpResponse.create(200, responseContent);
+                    });
+        } catch (Exception e){
+            var responseContent = Serialization.serialize(e.getMessage());
+
+            return HttpResponse.create(400,responseContent);
+        }
+    }
+}

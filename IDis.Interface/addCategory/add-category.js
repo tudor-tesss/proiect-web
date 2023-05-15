28b3ed6767dd612214b3f8f7ba7809f4ce4cf425
Stirbu.Larisa <eusuntlarisa@gmail.com>
2023-05-15 17:59:02 +0300

    almost frontend

--- IDis.Interface/addCategory/add-category.js
+++ IDis.Interface/addCategory/add-category.js
@@ -1,21 +1,66 @@
 var numberOfFieldsDropdown = document.getElementById('numberOfFields');
 numberOfFieldsDropdown.addEventListener('change', generateTextInputFields);
-generateTextInputFields()
+generateTextInputFields();
 
 function generateTextInputFields() {
-    var numberOfFields = document.getElementById("numberOfFields").value;
-    var textInputFields = document.getElementById("rating-fields");
-  
-    // clear the div where the text input fields will be inserted
+    var numberOfFields = parseInt(numberOfFieldsDropdown.value);
+    var textInputFields = document.getElementById("input-field");
+
+    // Clear the div where the text input fields will be inserted
     textInputFields.innerHTML = "";
-  
-    // generate a dynamic number of text input fields based on the selected value
+
+    // Generate a dynamic number of text input fields based on the selected value
     for (var i = 1; i <= numberOfFields; i++) {
-      var input = document.createElement("input");
-      input.classList.add("input")
-      input.type = "text";
-      input.placeholder = "Rating " + i;
-      input.name = "rating-" + i;
-      textInputFields.appendChild(input);
+        var inputContainer = document.createElement("div");
+        inputContainer.classList.add("name-label", "input-field");
+
+        var input = document.createElement("input");
+        input.type = "text";
+        input.placeholder = "Rating " + i;
+        input.name = "rating-" + i;
+        input.id = "rating" +i;
+        input.required = true;
+        input.classList.add("input-field");
+
+
+        inputContainer.appendChild(input);
+        textInputFields.appendChild(inputContainer);
     }
-  }
+}
+
+function displayCategoryForm() {
+    const categoryForm = document.querySelector('.category-form');
+    categoryForm.innerHTML = `
+    <div class="category-box">
+      <h2>Add Category</h2>
+      <form>
+        <div class="name-label">
+          <input type="text" class="input" id="category-name" required>
+          <label>Name</label>
+        </div>
+        <div class="input-field">
+        
+          <label>Choose the number of rating fields:</label>
+          
+          <div class="selection-options" id="selection-options">
+            <select id="numberOfFields" name="number">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+        <div id="input-field"></div>
+        <div id="error-container" class="error-container"></div>
+        <div class="submit-wrapper">
+          <button type="button" class="submit-b" onClick="createUser()">Submit</button>
+        </div>
+      </form>
+    </div>
+  `;
+
+    // Generate the initial set of input fields after adding form content to the DOM
+    generateTextInputFields();
+}

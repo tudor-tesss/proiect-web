9b5706147694891434dd97ed864fed891689d212
Tudor Tescu <tudor.tescu@hotmail.com>
2023-05-18 23:39:28 +0300

    add svg icon because its cute

--- IDis.Interface/addCategory/add-category.js
+++ IDis.Interface/addCategory/add-category.js
@@ -67,30 +67,31 @@ errorMessages = {
     "Category.RatingField.NullOrEmpty": "Rating field cannot be empty.",
 }
 
-
 function updateTextInputFields() {
-    var textInputFields = document.getElementById("input-field");
+  var textInputFields = document.getElementById("input-field");
 
-    // Clear the div where the text input fields will be inserted
-    textInputFields.innerHTML = "";
+  // Clear the div where the text input fields will be inserted
+  textInputFields.innerHTML = "";
 
-    // Generate the initial set of text input fields
-    for (var i = 1; i <= 1; i++) {
-        var inputContainer = document.createElement("div");
-        inputContainer.classList.add("input-field");
+  // Generate the initial set of text input fields
+  for (var i = 1; i <= 1; i++) {
+      var inputContainer = document.createElement("div");
+      inputContainer.classList.add("input-field");
 
-        var input = document.createElement("input");
-        input.type = "text";
-        input.placeholder = "New Rating Field ";
-        input.name = "rating-" + i;
-        input.required = true;
-        input.classList.add("rating-field");
+      var input = document.createElement("input");
+      input.type = "text";
+      input.placeholder = "New Rating Field ";
+      input.name = "rating-" + i;
+      input.required = true;
+      input.classList.add("rating-field");
 
-        inputContainer.appendChild(input);
-        textInputFields.appendChild(inputContainer);
-    }
+      var deleteButton = createDeleteButton(); 
+
+      inputContainer.appendChild(input);
+      inputContainer.appendChild(deleteButton);
+      textInputFields.appendChild(inputContainer);
+  }
 }
-
 
 function displayCategoryForm() {
     const categoryForm = document.querySelector('.category-form');
@@ -104,7 +105,6 @@ function displayCategoryForm() {
       <div id="input-field"></div>
       <div class="btn-wrapper">
         <button type="button" class="add-field-btn" onClick="addField()">Add</button>
-        <button type="button" class="remove-field-btn" onClick="removeField()">Remove</button>
       </div>
       <div id="error-container" class="error-container"></div>
       <div class="submit-wrapper">
@@ -115,6 +115,24 @@ function displayCategoryForm() {
 
     // Generate the initial set of input fields after adding form content to the DOM
     updateTextInputFields();
+}
+
+function createDeleteButton() {
+    var deleteButton = document.createElement("button");
+    deleteButton.type = "button";
+    deleteButton.classList.add("remove-field-btn");
+
+    deleteButton.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24" fill="currentColor">
+            <path d="M261 936q-24.75 0-42.375-17.625T201 876V306h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438V306ZM367 790h60V391h-60v399Zm166 0h60V391h-60v399ZM261 306v570-570Z"/>
+        </svg>  
+    `;
+
+    deleteButton.onclick = function() {
+        this.parentNode.remove();
+    };
+
+    return deleteButton;
 }
 
 function addField() {
@@ -128,7 +146,10 @@ function addField() {
     input.required = true;
     input.classList.add("rating-field");
 
+    var deleteButton = createDeleteButton();
+
     inputContainer.appendChild(input);
+    inputContainer.appendChild(deleteButton);
 
     var inputField = document.getElementById("input-field");
     inputField.appendChild(inputContainer);

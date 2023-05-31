5e7408fe568184d9f0379958be404628a50be524
Tudor Tescu <tudor.tescu@hotmail.com>
2023-05-31 15:26:46 +0300

    fixed wrong commit on main branch :)

--- IDis.Interface/categories/add/add-category.js
+++ IDis.Interface/categories/add/add-category.js
@@ -1,156 +1,127 @@
-async function createCategory() {
-    const endpoint = "http://localhost:7101/categories";
+import { AuthenticationService, CategoriesService } from "../../@shared/index.js";
 
-    const name = document.getElementById("category-name").value;
-    var inputs = document.querySelectorAll(".rating-field");
-    var ratingFields = Array.from(inputs).map((input) => input.value);
+window.AuthenticationService = AuthenticationService;
+await AuthenticationService.checkSession();
 
-    console.log(ratingFields);
+export class AddCategoryComponent {
+    static async createCategory() {
+        const name = document.getElementById("category-name").value;
+        const inputs = document.querySelectorAll(".rating-field");
+        const ratingFields = Array.from(inputs).map((input) => input.value);
+        const creatorId = localStorage.getItem("userUuid");
 
-    var creatorId = localStorage.getItem("userUuid");
+        await CategoriesService
+            .createCategory(name, ratingFields, creatorId)
+            .then(() => {
+                window.location.href = "/main/main.html";
+            })
+            .catch((error) => {
+                this.displayError(this.errorMessages[error.message]);
+            });
+    }
 
-    const createCategoryCommand = {
-        name: name,
-        ratingFields: ratingFields,
-        creatorId: creatorId,
-    };
+    static updateTextInputFields() {
+        let textInputFields = document.getElementById("input-field");
 
-    await fetch(endpoint, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(createCategoryCommand),
-    })
-    .then(async (response) => {
-        if (!response.ok) {
-            const error = await response.json();
-            throw new Error(error);
+        textInputFields.innerHTML = "";
+
+        for (let i = 1; i <= 1; i++) {
+            const inputContainer = document.createElement("div");
+            inputContainer.classList.add("input-field");
+
+            const input = document.createElement("input");
+            input.type = "text";
+            input.placeholder = "New Rating Field ";
+            input.name = "rating-" + i;
+            input.required = true;
+            input.classList.add("rating-field");
+
+            const deleteButton = this.createDeleteButton();
+
+            inputContainer.appendChild(input);
+            inputContainer.appendChild(deleteButton);
+            textInputFields.appendChild(inputContainer);
         }
+    }
 
-        return response.json();
-    })
-    .then(() => {
-        window.location.href = "../main/add-post.html";
-    })
-    .catch((error) => {
-        console.error("Error:", error);
+    static displayCategoryForm() {
+        const categoryForm = document.querySelector(".category-form");
+        categoryForm.innerHTML = `
+            <div class="category-box">
+              <h2>Add Category</h2>
+              <div class="name-label">
+                <input type="text" class="input" id="category-name" required>
+                <label>Name</label>
+              </div>
+              <div id="input-field"></div>
+              <div class="btn-wrapper">
+                <button type="button" class="add-field-btn" onClick="AddCategoryComponent.addField()">Add</button>
+              </div>
+              <div id="error-container" class="error-container"></div>
+              <div class="submit-wrapper">
+                <button type="button" class="submit-b" onClick="AddCategoryComponent.createCategory()">Submit</button>
+              </div>
+            </div>
+        `;
 
-        var errorMessage = error.message;
+        this.updateTextInputFields();
+    }
 
-        if (errorMessages[errorMessage] == undefined) {
-            errorMessage = "An error occurred while creating your category.";
-        }
-        displayError(errorMessages[errorMessage]);
-    });
-}
+    static createDeleteButton() {
+        let deleteButton = document.createElement("button");
+        deleteButton.type = "button";
+        deleteButton.classList.add("remove-field-btn");
 
-function displayError(errorMessage) {
-    const errorContainer = document.getElementById("error-container");
-    errorContainer.textContent = errorMessage;
-    errorContainer.style.display = "block";
+        deleteButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24" fill="currentColor">
+                <path d="M261 936q-24.75 0-42.375-17.625T201 876V306h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438V306ZM367 790h60V391h-60v399Zm166 0h60V391h-60v399ZM261 306v570-570Z"/>
+            </svg>  
+        `;
 
-    setTimeout(() => {
-        errorContainer.style.display = "none";
-    }, 5000);
-}
+        deleteButton.onclick = function()
+        {
+            this.parentNode.remove();
+        };
 
-errorMessages = {
-    "Category.AlreadyExists": "There is already a category with the same name.",
-    "Category.Name.NullOrEmpty": "Name is required.",
-    "Category.RatingField.NullOrEmpty": "Rating field cannot be empty.",
-};
+        return deleteButton;
+    }
 
-function updateTextInputFields() {
-    var textInputFields = document.getElementById("input-field");
-
-    textInputFields.innerHTML = "";
-
-    for (var i = 1; i <= 1; i++) {
-        var inputContainer = document.createElement("div");
+    static addField() {
+        const inputContainer = document.createElement("div");
         inputContainer.classList.add("input-field");
 
-        var input = document.createElement("input");
+        const input = document.createElement("input");
         input.type = "text";
-        input.placeholder = "New Rating Field ";
-        input.name = "rating-" + i;
+        input.placeholder = "New Rating Field";
+        input.name = "rating";
         input.required = true;
         input.classList.add("rating-field");
 
-        var deleteButton = createDeleteButton();
+        const deleteButton = this.createDeleteButton();
 
         inputContainer.appendChild(input);
         inputContainer.appendChild(deleteButton);
-        textInputFields.appendChild(inputContainer);
+
+        const inputField = document.getElementById("input-field");
+        inputField.appendChild(inputContainer);
     }
-}
 
-function displayCategoryForm() {
-    const categoryForm = document.querySelector(".category-form");
-    categoryForm.innerHTML = `
-    <div class="category-box">
-      <h2>Add Category</h2>
-      <div class="name-label">
-        <input type="text" class="input" id="category-name" required>
-        <label>Name</label>
-      </div>
-      <div id="input-field"></div>
-      <div class="btn-wrapper">
-        <button type="button" class="add-field-btn" onClick="addField()">Add</button>
-      </div>
-      <div id="error-container" class="error-container"></div>
-      <div class="submit-wrapper">
-        <button type="button" class="submit-b" onClick="createCategory()">Submit</button>
-      </div>
-    </div>
-  `;
-
-    updateTextInputFields();
-}
-
-function createDeleteButton() {
-    var deleteButton = document.createElement("button");
-    deleteButton.type = "button";
-    deleteButton.classList.add("remove-field-btn");
-
-    deleteButton.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 96 960 960" width="24" fill="currentColor">
-            <path d="M261 936q-24.75 0-42.375-17.625T201 876V306h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438V306ZM367 790h60V391h-60v399Zm166 0h60V391h-60v399ZM261 306v570-570Z"/>
-        </svg>  
-    `;
-
-    deleteButton.onclick = function () {
-        this.parentNode.remove();
+    static errorMessages = {
+        "Category.AlreadyExists": "There is already a category with the same name.",
+        "Category.Name.NullOrEmpty": "Name is required.",
+        "Category.RatingField.NullOrEmpty": "Rating field cannot be empty.",
     };
 
-    return deleteButton;
-}
+    static displayError(errorMessage) {
+        const errorContainer = document.getElementById("error-container");
+        errorContainer.textContent = errorMessage;
+        errorContainer.style.display = "block";
 
-function addField() {
-    var inputContainer = document.createElement("div");
-    inputContainer.classList.add("input-field");
-
-    var input = document.createElement("input");
-    input.type = "text";
-    input.placeholder = "New Rating Field";
-    input.name = "rating";
-    input.required = true;
-    input.classList.add("rating-field");
-
-    var deleteButton = createDeleteButton();
-
-    inputContainer.appendChild(input);
-    inputContainer.appendChild(deleteButton);
-
-    var inputField = document.getElementById("input-field");
-    inputField.appendChild(inputContainer);
-}
-
-function removeField() {
-    var inputField = document.getElementById("input-field");
-    var inputContainers = inputField.getElementsByClassName("input-field");
-
-    if (inputContainers.length > 1) {
-        inputField.removeChild(inputContainers[inputContainers.length - 1]);
+        setTimeout(() => {
+            errorContainer.style.display = "none";
+        }, 5000);
     }
 }
+
+window.AddCategoryComponent = AddCategoryComponent;
+AddCategoryComponent.displayCategoryForm();

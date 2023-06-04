ccccc049846638b56c006f70c6f1baf65aae2824
Tudor Tescu <tudor.tescu@hotmail.com>
2023-06-04 14:16:44 +0300

    handler tests

--- IDis.Backend/IDis/src/test/java/com/idis/core/business/user/CreateUserCommandHandlerTests.java
+++ IDis.Backend/IDis/src/test/java/com/idis/core/business/user/CreateUserCommandHandlerTests.java
@@ -3,53 +3,81 @@ package com.idis.core.business.user;
 import com.idis.core.business.BusinessErrors;
 import com.idis.core.business.user.commandhandlers.CreateUserCommandHandler;
 import com.idis.core.business.user.commands.CreateUserCommand;
+import com.idis.core.domain.DomainErrors;
 import com.idis.core.domain.user.User;
-import com.nimblej.networking.database.Database;
 import com.nimblej.networking.database.NimbleJQueryProvider;
 import org.junit.jupiter.api.Test;
-import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
-import java.sql.SQLException;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.ArgumentMatchers.any;
 
-@PrepareForTest({NimbleJQueryProvider.class, Database.class})
 public class CreateUserCommandHandlerTests {
-    static {
-
-
-        try {
-            NimbleJQueryProvider.initiate("test", "test2", "test3422");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     @Test
     public void when_emailAddressIsAlreadyTaken_then_shouldFail() {
         // Arrange
         var command = command();
         var user = User.create(command.name(), command.firstName(), command.emailAddress());
 
-        try (var mockStatic = mockStatic(NimbleJQueryProvider.class)) {
-            mockStatic.when(() -> NimbleJQueryProvider.getAll(User.class)).thenReturn(List.of(user));
-            doNothing().when(NimbleJQueryProvider.class);
-            NimbleJQueryProvider.insert(user);
+        try (MockedStatic<NimbleJQueryProvider> mock = Mockito.mockStatic(NimbleJQueryProvider.class)) {
+            mock.when(() -> NimbleJQueryProvider.getAll(User.class)).thenReturn(List.of(user));
+            mock.when(() -> NimbleJQueryProvider.insert(any(User.class))).thenAnswer(invocation -> null);
+
+            // Act
+            var exception = assertThrows(IllegalArgumentException.class, () -> {
+                sut().handle(command);
+            });
+
+            // Assert
+            var actualMessage = exception.getMessage();
+
+            assertTrue(actualMessage.contains(BusinessErrors.User.UserAlreadyExists));
         }
+    }
 
-        // Act
-        var exception = assertThrows(IllegalArgumentException.class, () -> {
-            sut().handle(command);
-        });
+    @Test
+    public void when_domainFails_then_shouldFail() {
+        // Arrange
+        var command = new CreateUserCommand(null, "test", "test");
 
-        // Assert
-        var actualMessage = exception.getMessage();
+        try (MockedStatic<NimbleJQueryProvider> mock = Mockito.mockStatic(NimbleJQueryProvider.class)) {
+            mock.when(() -> NimbleJQueryProvider.getAll(User.class)).thenReturn(List.of());
+            mock.when(() -> NimbleJQueryProvider.insert(any(User.class))).thenAnswer(invocation -> null);
 
-        assertTrue(actualMessage.contains(BusinessErrors.User.UserAlreadyExists));
+            // Act
+            var exception = assertThrows(IllegalArgumentException.class, () -> {
+                sut().handle(command);
+            });
+
+            // Assert
+            var actualMessage = exception.getMessage();
+
+            assertTrue(actualMessage.contains(DomainErrors.User.NameNullOrEmpty));
+        }
+    }
+
+    @Test
+    public void when_domainSucceeds_then_shouldSucceed() throws ExecutionException, InterruptedException {
+        // Arrange
+        var command = command();
+
+        try (MockedStatic<NimbleJQueryProvider> mock = Mockito.mockStatic(NimbleJQueryProvider.class)) {
+            mock.when(() -> NimbleJQueryProvider.getAll(User.class)).thenReturn(List.of());
+            mock.when(() -> NimbleJQueryProvider.insert(any(User.class))).thenAnswer(invocation -> null);
+
+            // Act
+            var result = sut().handle(command).get();
+
+            // Assert
+            assertTrue(result.getName().equals(command.name()));
+            assertTrue(result.getFirstName().equals(command.firstName()));
+            assertTrue(result.getEmailAddress().equals(command.emailAddress()));
+        }
     }
 
     private CreateUserCommandHandler sut() {
